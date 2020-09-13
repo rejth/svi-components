827f24a6d4154ba47fc2602e913e0c8a2611d4e2
rusiki <kirsanovia@yandex.ru>
2020-09-13 11:54:44 +0300

    Added load_data_to_table.sql

--- data-from-sas-job/scripts/load_data_to_table.sql
+++ data-from-sas-job/scripts/load_data_to_table.sql
@@ -9,17 +9,17 @@ insert into etl_stg.not_fraud_transactions (
     "EMP_NUM"
 )
 select
-    sr."ACC_FROM",
-    sr."ACC_TO",
-    coalesce(sr."CLFIO_D", vcl_1."CLIENT_FIO"),
-    coalesce(sr."CLFIO_C", vcl_2."CLIENT_FIO"),
-    sr."TRNX_CUR",
-    sr."TRNX_SR",
-    sr."TRNX_DTTM",
-    sr."TRNX_EMPL",
+    sr."acc_from",
+    sr."acc_to",
+    coalesce(sr."clfio_d", vcl_1."CLIENT_FIO"),
+    coalesce(sr."clfio_c", vcl_2."CLIENT_FIO"),
+    sr."trnx_cur",
+    sr."trnx_sr",
+    sr."trnx_dttm",
+    sr."trnx_empl"
 from etl_stg.scoring_result sr
 left join etl_stg.vi_client vcl_1
-    on sr."CLI_ID_D" = vcl_1."CLIENT_ID"
+    on sr.cli_id_d = vcl_1."CLIENT_ID"
 left join etl_stg.vi_client vcl_2
-    on sr."CLI_ID_C" = vcl_2."CLIENT_ID"
+    on sr.cli_id_c = vcl_2."CLIENT_ID"
 where fraud_flag = 0
